560f1da0a620bd1f38772097fe3f06191ca67bcc
varyl97 <86377967+varyl97@users.noreply.github.com>
2021-10-12 10:38:24 -0700

    Update Coding_Demo.R

--- Coding_Demo.R
+++ Coding_Demo.R
@@ -1,1 +1,29 @@
 ## Analytical Workflows Coding Demo - 10/12/2021
+#coding best practices 
+
+
+#insert additional information here that may be useful to collabs or future self
+#    here, we will generate data, do linear regression, and make some figures
+#section headers! Can write with ctr+shift+r 
+#clicking source on save RUNS through the entire script so you know what you 
+#    save will be what you think and will run without errors 
+
+# Clear workspace and close all graphics devices --------------------------
+rm(list=ls())
+graphics.off()
+
+# Generate some data ------------------------------------------------------
+
+n<-100 #number of data points 
+m<-2.2 #slope
+b<-1.1 #intercept 
+
+x<-rnorm(100, mean=0, sd=1) #indep. variable 
+noise<-rnorm(100,mean=0,sd=1)  
+y<-m*x+b+noise #linear relationship + noise 
+
+plot(x,y)
+
+
+
+
